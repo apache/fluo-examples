80e10334356ea2da9bb6cf19d9e9404785088600
Kenneth McFarland <kennethmcfarland@apache.org>
2019-03-15 11:44:28 -0700

    Refactoring of PageLoaderTest
    Added Apache License header
    Renamed methods with better descriptives
    Removed unused local variable warnings

--- webindex/modules/data/src/test/java/webindex/data/fluo/PageLoaderTest.java
+++ webindex/modules/data/src/test/java/webindex/data/fluo/PageLoaderTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package webindex.data.fluo;
 
 import java.net.MalformedURLException;
@@ -8,42 +25,41 @@ import org.junit.Test;
 import webindex.core.models.Page;
 import webindex.core.models.URL;
 
-@SuppressWarnings("unused")
+
 public class PageLoaderTest {
 
 	@Test
 	public void testUpdatePageWithNullPage() {
-		Page p = null;
-		PageLoader loader = PageLoader.updatePage(p);
-		loader = null;
+		PageLoader.updatePage((Page) null);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testEmptyPageThrowsIllegalArgument() {
 		Page p = Page.EMPTY;
-		PageLoader loader = PageLoader.updatePage(p);
-		loader = null;
+		PageLoader.updatePage(p);
 	}
 	
 	@Test(expected = NullPointerException.class)
-	public void testDeleteFailsWithNullUrl() {
+	public void testDeletePageThrowsNPEifNullURL() {
 		try {
-			PageLoader loader = PageLoader.deletePage(null);
+			PageLoader.deletePage((URL)null);
 		} catch (MalformedURLException e) {
-			e.printStackTrace();
+			
 		}
+		
 	}
 	
 	@Test
-	public void testDeletePage() {
+	public void testDeletePageWithBlankURL() {
 		URL url = new URL("","","",0,false,false);
 		try {
-			PageLoader loader = PageLoader.deletePage(url);
+			PageLoader.deletePage(url);
 		} catch (MalformedURLException e) {
-			e.printStackTrace();
+			
 		}
 	}
 	
+	//This test wont make sense until PageLoader.deletePage() handles non null empty URLS
 	@Test
 	public void testLoadWithNullTxCtx() {
 		URL url = new URL("","","",0,false,false);
@@ -51,13 +67,12 @@ public class PageLoaderTest {
 		try {
 			loader = PageLoader.deletePage(url);
 			loader.load(null, null);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (NullPointerException e) {
-			e.printStackTrace();
+		} catch (NullPointerException e) {	
 			Assert.fail();
+		} catch (MalformedURLException e) {
+			// from PageLoader.deletePage()
 		} catch (Exception e) {
-			e.printStackTrace();
-		} 
+			// from PageLoader.load()
+		}
 	}
 }
